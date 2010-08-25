5b6abf9b1841eb9c8be4fb224eece22f0dd5cfd3
Peter Hofmann <scm@uninformativ.de>
2010-08-25 13:59:45 +0200

    Consider this script to be pizza-ware. ;-)

--- explain.py
+++ explain.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # coding: utf8
 
+# ------------------------------------------------------------------
+# "THE PIZZA-WARE LICENSE" (Revision 42):
+# Peter Hofmann <pcode@uninformativ.de> wrote this file. As long as you
+# retain this notice you can do whatever you want with this stuff. If
+# we meet some day, and you think this stuff is worth it, you can buy
+# me a pizza in return.
+# ------------------------------------------------------------------
+
 import sys
 import textwrap
 from optparse import OptionParser
