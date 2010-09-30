fa3e3fb75baec3b43f00d2a22bf1b9e84913c65e
Peter Hofmann <scm@uninformativ.de>
2010-09-30 20:11:33 +0200

    Refactoring: Made it a class + minor cleanup.
    Okay, almost the final step. Now there's a class "Explainer" that does
    all the work. It offers a method called "explain(text)" that explains
    the given text. Options can be set as member variables.
    
    As a result, we get a nice and clean API:
    
    	import explain
    
    	mystr = """... explain-syntax ..."""
    
    	explainer = explain.Explainer()
    	explainer.straight = '!'                # optional, of course
    	expl_list = explainer.explain(mystr)
    	print '\n'.join(expl_list),
    
    As a bonus, it's now easier to avoid all that "adding trailing
    newlines"-foo.

--- explain.py
+++ explain.py
@@ -13,229 +13,247 @@ import sys
 import textwrap
 from optparse import OptionParser
 
-def parse_plaintext_explanation(content):
-    """Find out which ranges the comments apply to.
+class Explainer(object):
+    def __init__(self):
+        """Set default options."""
 
-    Returns a list of tuples.  Each tuple contains one command and the
-    list of associated comments (including the range for each comment).
-    """
+        self.line_len = 72
+        self.set_ascii_symbols()
 
-    # Split and filter lines.  The last empty line ensures that the end
-    # of the last comment will be detected.
-    lines = content.split('\n')
-    lines = [i for i in lines if len(i) == 0 or i[0] != ';']
-    lines += ['']
+    def set_ascii_symbols(self):
+        """Set ASCII symbols."""
 
-    all_explanations = []
-    while len(lines) > 0:
-        # The command itself is in the very first line.
-        cmd = lines.pop(0)
-        if cmd == '':
-            continue
+        self.corner = '\\- '
+        self.straight = '|'
+        self.ranges = '\\_/'
+        self.joints = '_'
 
-        # Read the markers and store their ranges.
-        markers = lines.pop(0) + ' '
-        indexes = []
-        start = -1
-        i = 0
-        for i in range(len(markers)):
-            c = markers[i]
-            if c == '-' and start == -1:
-                start = i
-            elif c == ' ' and start != -1:
-                indexes += [(start, i - start)]
-                start = -1
-            elif c == '+' and start != -1:
-                indexes += [(start, i - start + 1)]
-                start = -1
-            elif c == '!':
-                if start != -1:
+    def set_unicode_symbols(self):
+        """Set unicode symbols."""
+
+        self.corner = u'└ '
+        self.straight = u'│'
+        self.ranges = u'└─┘'
+        self.joints = u'┬'
+
+    def explain(self, text):
+        """Parse and annotate the given text.
+
+        This is meant to be the method that's used by you.
+
+        Returns a list of annotated commands.
+        """
+
+        explained = []
+        indexed = self.parse_plaintext_explanation(text)
+        for (cmd, indexed_comments) in indexed:
+            explained += [self.annotate_indexed_comments(cmd,
+                indexed_comments)]
+        return explained
+
+    def parse_plaintext_explanation(self, content):
+        """Find out which ranges the comments apply to.
+
+        Returns a list of tuples.  Each tuple contains one command and
+        the list of associated comments (including the range for each
+        comment).
+        """
+
+        # Split and filter lines.  The last empty line ensures that the
+        # end of the last comment will be detected.
+        lines = content.split('\n')
+        lines = [i for i in lines if len(i) == 0 or i[0] != ';']
+        lines += ['']
+
+        all_explanations = []
+        while len(lines) > 0:
+            # The command itself is in the very first line.
+            cmd = lines.pop(0)
+            if cmd == '':
+                continue
+
+            # Read the markers and store their ranges.
+            markers = lines.pop(0) + ' '
+            indexes = []
+            start = -1
+            i = 0
+            for i in range(len(markers)):
+                c = markers[i]
+                if c == '-' and start == -1:
+                    start = i
+                elif c == ' ' and start != -1:
                     indexes += [(start, i - start)]
-                indexes += [(i, 0)]
-                start = -1
+                    start = -1
+                elif c == '+' and start != -1:
+                    indexes += [(start, i - start + 1)]
+                    start = -1
+                elif c == '!':
+                    if start != -1:
+                        indexes += [(start, i - start)]
+                    indexes += [(i, 0)]
+                    start = -1
 
-        # Extract comments.
-        comments = []
-        one_comment = []
-        at = 0
-        while len(lines) > 0 and at < len(indexes):
-            line = lines.pop(0).strip()
-            if line == '' and len(one_comment) > 0:
-                one_comment = ' '.join(one_comment)
-                comments += [one_comment]
-                one_comment = []
-                at += 1
+            # Extract comments.
+            comments = []
+            one_comment = []
+            at = 0
+            while len(lines) > 0 and at < len(indexes):
+                line = lines.pop(0).strip()
+                if line == '' and len(one_comment) > 0:
+                    one_comment = ' '.join(one_comment)
+                    comments += [one_comment]
+                    one_comment = []
+                    at += 1
 
-            elif line != '':
-                one_comment += [line]
+                elif line != '':
+                    one_comment += [line]
 
-        # No comments or no indices were found.
-        if len(comments) == 0:
-            all_explanations += [(cmd, None)]
-            continue
+            # No comments or no indices were found.
+            if len(comments) == 0:
+                all_explanations += [(cmd, None)]
+                continue
 
-        # Associate comments with their ranges.
-        indexed_comments = zip(indexes, comments)
-        indexed_comments = map(lambda ((start, length), comment):
-                (start, length, comment), indexed_comments)
-        indexed_comments.reverse()
+            # Associate comments with their ranges.
+            indexed_comments = zip(indexes, comments)
+            indexed_comments = map(lambda ((start, length), comment):
+                    (start, length, comment), indexed_comments)
+            indexed_comments.reverse()
 
-        all_explanations += [(cmd, indexed_comments)]
+            all_explanations += [(cmd, indexed_comments)]
 
-    return all_explanations
+        return all_explanations
 
-def explain(options, cmd, indexed_comments):
-    """Explain one single command.
+    def annotate_indexed_comments(self, cmd, indexed_comments):
+        """Explain one single command.
 
-    Given the desired options, a command and a list of indexed comments,
-    explain the command by drawing lines from the comments to the
-    associated parts of the command.  Line length and symbols for the
-    graph are stored in the dictionary "options".
+        Given the desired options, a command and a list of indexed
+        comments, explain the command by drawing lines from the comments
+        to the associated parts of the command.  Line length and symbols
+        for the graph are stored in the dictionary "options".
 
-    Returns the annotated command as a string.
-    """
+        Returns the annotated command as a string.
+        """
 
-    if indexed_comments is None:
-        return cmd + '\n'
+        if indexed_comments is None:
+            return cmd + '\n'
 
-    # Save some typing...
-    line_len = options['line_len']
-    corner = options['corner']
-    straight = options['straight']
-    ranges = options['ranges']
-    joints = options['joints']
+        # We *need* the lines to be at least as long as the command.
+        # Additionally, add some space to the right which is needed for
+        # the comments.
+        line_len = max(self.line_len, len(cmd) + 10)
 
-    # We *need* the lines to be at least as long as the command.
-    # Additionally, add some space to the right which is needed for the
-    # comments.
-    line_len = max(line_len, len(cmd) + 10)
+        # Our "drawing" will start with an initial empty line.
+        empty_line = ' ' * line_len
+        drawing = empty_line + '\n'
+        y = 1
 
-    # Our "drawing" will start with an initial empty line.
-    empty_line = ' ' * line_len
-    drawing = empty_line + '\n'
-    y = 1
-
-    # If the comment on the very right is associated with a range
-    # greater than 2, then add an additional empty line.  This is done
-    # because we want to show a "|" over each corner.  If we didn't add
-    # the extra line, the "|" would be missing.
-    if indexed_comments[0][1] > 2:
-        drawing += empty_line + '\n'
-        y += 1
-
-    # Add the corner symbol and the wrapped comment.  Keep track of
-    # where we placed the corners.  Every line will be exactly
-    # "line_len" characters long.  This allows us to draw arrows and
-    # stuff later on.
-    corners = []
-    for i in range(len(indexed_comments)):
-        (start, length, comment) = indexed_comments[i]
-
-        # Don't move the graph to the right if length is too short.
-        skip = 0
-        if length >= 3:
-            skip = length / 2
-
-        # The indented corner.
-        first_indent = ' ' * start
-        first_indent += ' ' * skip
-        first_indent += corner
-        drawing += first_indent
-        y += 1
-
-        # Remember this corner.
-        corners += [(start + skip, y)]
-
-        # Wrap the comment and add its first line.
-        comment_width = line_len - start - skip - len(corner)
-        wrapped = textwrap.wrap(comment, comment_width)
-        drawing += wrapped.pop(0).ljust(comment_width) + '\n'
-
-        # Add remaining lines.  All of them have to be properly indented.
-        for line in wrapped:
-            drawing += ' ' * (start + skip + len(corner))
-            drawing += line.ljust(comment_width)
-            drawing += '\n'
-            y += 1
-
-        # If this is not the very last comment, then add an extra line.
-        if i < len(indexed_comments) - 1:
+        # If the comment on the very right is associated with a range
+        # greater than 2, then add an additional empty line.  This is
+        # done because we want to show a "|" over each corner.  If we
+        # didn't add the extra line, the "|" would be missing.
+        if indexed_comments[0][1] > 2:
             drawing += empty_line + '\n'
             y += 1
 
-    # Draw the lines from the corners up to the command.
-    drawing_list = list(drawing)
-    for x, y in corners:
-        for i in range(y - 1):
-            drawing_list[i * (line_len + 1) + x] = straight
+        # Add the corner symbol and the wrapped comment.  Keep track of
+        # where we placed the corners.  Every line will be exactly
+        # "line_len" characters long.  This allows us to draw arrows and
+        # stuff later on.
+        corners = []
+        for i in range(len(indexed_comments)):
+            (start, length, comment) = indexed_comments[i]
 
-    # Draw ranges if they're greater 2.
-    for (start, length, _) in indexed_comments:
-        if length < 3:
-            continue
+            # Don't move the graph to the right if length is too short.
+            skip = 0
+            if length >= 3:
+                skip = length / 2
 
-        drawing_list[start] = ranges[0]
-        drawing_list[start + length - 1] = ranges[2]
+            # The indented corner.
+            first_indent = ' ' * start
+            first_indent += ' ' * skip
+            first_indent += self.corner
+            drawing += first_indent
+            y += 1
 
-        for i in range(start + 1, start + length - 1):
-            drawing_list[i] = ranges[1]
+            # Remember this corner.
+            corners += [(start + skip, y)]
 
-        drawing_list[start + length / 2] = joints
+            # Wrap the comment and add its first line.
+            comment_width = line_len - start - skip - len(self.corner)
+            wrapped = textwrap.wrap(comment, comment_width)
+            drawing += wrapped.pop(0).ljust(comment_width) + '\n'
 
-    # Convert it back to a string.
-    drawing = ''.join(drawing_list)
-    explained = cmd + '\n' + drawing
+            # Add remaining lines.  All of them have to be properly
+            # indented.
+            for line in wrapped:
+                drawing += ' ' * (start + skip + len(self.corner))
+                drawing += line.ljust(comment_width)
+                drawing += '\n'
+                y += 1
 
-    # Remove any padding on the right hand.
-    lines = explained.split('\n')
-    lines = [i.rstrip() for i in lines]
-    explained = '\n'.join(lines)
-    return explained
+            # If this is not the very last comment, then add an extra
+            # line.
+            if i < len(indexed_comments) - 1:
+                drawing += empty_line + '\n'
+                y += 1
 
-def get_default_options():
-    """Get the default set of symbols etc. as a dictionary."""
+        # Draw the lines from the corners up to the command.
+        drawing_list = list(drawing)
+        for x, y in corners:
+            for i in range(y - 1):
+                drawing_list[i * (line_len + 1) + x] = self.straight
 
-    options = {}
-    options['line_len'] = 72
-    options['corner'] = '\\- '
-    options['straight'] = '|'
-    options['ranges'] = '\\_/'
-    options['joints'] = '_'
-    return options
+        # Draw ranges if they're greater 2.
+        for (start, length, _) in indexed_comments:
+            if length < 3:
+                continue
 
-def apply_unicode_symbols(options):
-    """Keeps all options but applies the set of unicode symbols."""
+            drawing_list[start] = self.ranges[0]
+            drawing_list[start + length - 1] = self.ranges[2]
 
-    options['corner'] = u'└ '
-    options['straight'] = u'│'
-    options['ranges'] = u'└─┘'
-    options['joints'] = u'┬'
-    return options
+            for i in range(start + 1, start + length - 1):
+                drawing_list[i] = self.ranges[1]
+
+            drawing_list[start + length / 2] = self.joints
+
+        # Convert it back to a string.
+        drawing = ''.join(drawing_list)
+        explained = cmd + '\n' + drawing
+
+        # Remove any padding on the right hand.
+        lines = explained.split('\n')
+        lines = [i.rstrip() for i in lines]
+        explained = '\n'.join(lines)
+        return explained
 
 
 if __name__ == '__main__':
-    defaults = get_default_options()
+    # Create a new Explainer object.  Read its default settings and use
+    # them as the default command line options.
+    explainer = Explainer()
+
     parser = OptionParser(usage='usage: %prog [options] [file]...')
     parser.add_option('-w', '--width', dest='line_len',
                       help='Maximum width of output. Defaults to ' +
                       '%default.',
-                      default=defaults['line_len'], type='int')
+                      default=explainer.line_len, type='int')
     parser.add_option('-c', '--corners', dest='corner',
                       help='Characters to use as corners. Defaults ' +
-                      'to "%default".', default=defaults['corner'])
+                      'to "%default".', default=explainer.corner)
     parser.add_option('-s', '--straight', dest='straight',
                       help='Character to use as straight lines. ' +
                       'Defaults to "%default".',
-                      default=defaults['straight'])
+                      default=explainer.straight)
     parser.add_option('-r', '--ranges', dest='ranges',
                       help='Characters to use for ranges. Defaults ' +
-                      'to "%default".', default=defaults['ranges'])
+                      'to "%default".', default=explainer.ranges)
     parser.add_option('-j', '--joints', dest='joints',
                       help='Character to use for joints between ' +
                       'lines and ranges. Defaults to "%default".',
-                      default=defaults['joints'])
+                      default=explainer.joints)
     parser.add_option('-u', '--unicode', dest='unicode_preset',
-                      help='Use a preset of unicode glyphs for the graph.',
+                      help='Use a preset of unicode glyphs for the ' +
+                      'graph.',
                       default=False, action='store_true')
     parser.add_option('-8', '--dont-force-utf-8', dest='force_utf8',
                       help='Do not enforce UTF-8 as output encoding.',
@@ -243,47 +261,36 @@ if __name__ == '__main__':
 
     (options, args) = parser.parse_args()
 
-    # Pass options as a dictionary.
-    explain_options = {}
-    explain_options['line_len'] = options.line_len
-
     # Use unicode preset?  This will overwrite all other characters.
     # The other arguments have to be UTF-8-decoded, regardless.
     if options.unicode_preset:
-        apply_unicode_symbols(explain_options)
+        explainer.set_unicode_symbols()
     else:
-        explain_options['corner'] = options.corner.decode('UTF-8')
-        explain_options['straight'] = options.straight.decode('UTF-8')
-        explain_options['ranges'] = options.ranges.decode('UTF-8')
-        explain_options['joints'] = options.joints.decode('UTF-8')
+        explainer.corner = options.corner.decode('UTF-8')
+        explainer.straight = options.straight.decode('UTF-8')
+        explainer.ranges = options.ranges.decode('UTF-8')
+        explainer.joints = options.joints.decode('UTF-8')
 
-    # Read all files or stdin.
-    content = ''
+    explainer.line_len = options.line_len
+
+    # Read all files or stdin and annotate the commands.
     if len(args) > 0:
+        explained = []
         for i in args:
             try:
                 with open(i, 'r') as fp:
-                    # Ensure that the last line ends with a newline.
-                    # Also, an empty separating line is added.  This is
-                    # necessary because we need at least one empty line
-                    # between two files.
-                    content += fp.read().decode('UTF-8') + '\n\n'
-            except IOError as (errno, strerror):
-                print >> sys.stderr, 'Can\'t read %s: %s' % (i, strerror)
+                    content = fp.read().decode('UTF-8')
+                    explained += explainer.explain(content)
+            except IOError as (errno, errstr):
+                print >> sys.stderr, 'Can\'t read %s: %s' % (i, errstr)
                 sys.exit(1)
     else:
         try:
-            # No need to add anything here since there's *only* stdin
-            # and no other files.
-            content += sys.stdin.read().decode('UTF-8')
+            content = sys.stdin.read().decode('UTF-8')
+            explained = explainer.explain(content)
         except KeyboardInterrupt:
             sys.exit(1)
 
-    # Annotate commands.
-    parsed_lines = parse_plaintext_explanation(content)
-    explained = []
-    for (cmd, indexed_comments) in parsed_lines:
-        explained += [explain(explain_options, cmd, indexed_comments)]
     explained = '\n'.join(explained)
 
     # Enforce UTF-8?  This is needed when piping the output to another
